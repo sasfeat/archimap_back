f3b1286e93df61b721f374162aaab2559d7d543e
sasfeat <sasfeat@mail.ru>
2017-06-28 00:48:40 +0300

    initial commit

--- settings_heroku.py
+++ settings_heroku.py
@@ -3,6 +3,6 @@ webserver_port = 80
 
 db_name = 'd5c609fr9umu1v'
 db_port = 5432
-db_host = 'ec2-184-73-249-56.compute-1.amazonaws.com:'
+db_host = 'ec2-184-73-249-56.compute-1.amazonaws.com'
 db_user = 'sukaaseuyvubqs'
 db_pass = '3e2db1ca481efd090f9bcaca758e137826712bb94e9c2e0112bc89f7463dd312'
